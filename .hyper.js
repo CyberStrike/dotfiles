52f4b7e6fb05862d94c0c4d4035698b6eb2355f5
Chris Scott <cyberstrike@gmail.com>
2017-09-17 03:16:57 -0400

    added new plugins

--- .hyper.js
+++ .hyper.js
@@ -104,7 +104,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypertheme", "hyper-blink", "gitrocket"],
+  plugins: ["hypertheme", "hyper-blink", "gitrocket", "hypercwd"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
